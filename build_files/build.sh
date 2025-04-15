522a62894a8666ca57514242d9c495c96a459ae7
Amy/Atius <136548767+AtiusAmy@users.noreply.github.com>
2025-04-15 10:55:45 +0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,6 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-rsync -rvK /ctx/system_files/silverblue/ /
 
 # this installs a package from fedora repos
 dnf5 install -y gnome-shell-extension-appindicator gnome-shell-extension-caffeine tailscale android-tools gparted micro
