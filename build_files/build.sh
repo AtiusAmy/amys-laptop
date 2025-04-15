00362c72df1f94299c7daf6c11b36f8522d5b699
Amy/Atius <136548767+AtiusAmy@users.noreply.github.com>
2025-04-15 07:31:36 +0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,11 +12,13 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y gnome-shell-extension-appindicator gnome-shell-extension-caffeine tailscale android-tools gparted micro
 dnf5 -y copr enable antiderivative/libfprint-tod-goodix-0.0.9
+dnf5 remove -y libfprint 
 dnf5 -y swap \
     --repo copr:copr.fedorainfracloud.org:antiderivative:libfprint-tod-goodix-0.0.9 \
     libfprint-tod libfprint-tod
 dnf5 versionlock add libfprint-tod
 dnf5 intall -y libfprint-tod-goodix
+dnf5 install -y libfprint
 
 # Use a COPR Example:
 #
@@ -24,6 +26,7 @@ dnf5 intall -y libfprint-tod-goodix
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable antiderivative/libfprint-tod-goodix-0.0.9
 
 #### Example for enabling a System Unit File
 systemctl enable fprintd
