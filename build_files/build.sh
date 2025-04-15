f32dc9e6adb971b9efaead058b6fbef0021bd169
Amy/Atius <136548767+AtiusAmy@users.noreply.github.com>
2025-04-15 07:52:24 +0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -21,6 +21,5 @@ rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfraclou
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
-dnf5 -y copr disable antiderivative/libfprint-tod-goodix-0.0.9
 
 #### Example for enabling a System Unit File
