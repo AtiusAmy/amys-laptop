3c10ef356a0cb1858e3b8cd0f4487e2002bb349d
Amy/Atius <136548767+AtiusAmy@users.noreply.github.com>
2025-04-15 14:57:05 +0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,7 +15,7 @@ dnf5 install -y gnome-shell-extension-appindicator gnome-shell-extension-caffein
 dnf5 -y copr enable antiderivative/libfprint-tod-goodix-0.0.9
 curl -o /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:antiderivative:libfprint-tod-goodix-0.0.9.repo "https://copr.fedorainfracloud.org/coprs/antiderivative/libfprint-tod-goodix-0.0.9/repo/fedora-$(rpm -E %fedora)/antiderivative-libfprint-tod-goodix-0.0.9-fedora-$(rpm -E %fedora).repo"
 rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:antiderivative:libfprint-tod-goodix-0.0.9 --remove=libfprint libfprint-tod libfprint-tod-goodix
-
+dnf -y remove gnome-extension-app
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
