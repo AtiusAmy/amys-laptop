72d9500f8d83eb995200d27482674b70d7fd1f08
Amy/Atius <136548767+AtiusAmy@users.noreply.github.com>
2025-04-20 04:53:39 +0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,9 +9,10 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-
+dnf5 -y copr enable ublue-os/staging
+dnf5 -y copr enalbe ublue-os/packages
 # this installs a package from fedora repos
-dnf5 install -y gnome-shell-extension-appindicator gnome-shell-extension-caffeine gnome-shell-extension-blur-my-shell rclone tailscale android-tools gparted micro
+dnf5 install -y gnome-shell-extension-appindicator bluefin-fastfetch fastfetch gnome-shell-extension-logo-menu gnome-shell-extension-tailscale-gnome-qs gnome-shell-extension-caffeine gnome-shell-extension-blur-my-shell rclone tailscale android-tools gparted micro
 dnf5 -y copr enable antiderivative/libfprint-tod-goodix-0.0.9
 curl -o /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:antiderivative:libfprint-tod-goodix-0.0.9.repo "https://copr.fedorainfracloud.org/coprs/antiderivative/libfprint-tod-goodix-0.0.9/repo/fedora-$(rpm -E %fedora)/antiderivative-libfprint-tod-goodix-0.0.9-fedora-$(rpm -E %fedora).repo"
 rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:antiderivative:libfprint-tod-goodix-0.0.9 --remove=libfprint libfprint-tod libfprint-tod-goodix
@@ -24,5 +25,7 @@ dnf -y remove gnome-extensions-app
 # dnf5 -y copr disable ublue-os/staging
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/tailscale.repo
 dnf5 -y copr disable antiderivative/libfprint-tod-goodix-0.0.9
+dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable ublue-os/packages
 
 #### Example for enabling a System Unit File
