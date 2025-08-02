401d04a977e03a6fe6b41e528677d1cc777c44d4
AtiusAmy <torv.device027@passmail.net>
2025-08-02 12:53:15 +0700

    oops

--- build_files/build.sh
+++ build_files/build.sh
@@ -31,7 +31,7 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/tailscale.repo
 dnf5 -y copr disable antiderivative/libfprint-tod-goodix-0.0.9
 dnf5 -y copr disable ublue-os/staging
 dnf5 -y copr disable ublue-os/packages
-dnf5 -y copr disable kwizart/kernel-longterm-6.12
+
 
 #### Example for enabling a System Unit File
 echo "import \"/usr/share/ublue-os/just/amy.just\"" >> /usr/share/ublue-os/justfile
